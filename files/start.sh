1882a25bf349a80c849756abed657c3cf9426f5a
dbaltor <denis_baltoryahoo.com>
2018-11-12 00:59:47 +0000

    added docker image

--- files/start.sh
+++ files/start.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 set -e
-exec readfile.sh test1.csv | java -Djava.security.egd=file:/dev/./urandom -jar /app.jar both 100 2
+exec java -Djava.security.egd=file:/dev/./urandom -jar /app.jar both 100 2
